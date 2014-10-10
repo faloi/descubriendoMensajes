4d03af8df1949f4c3ee06e222be6cc7e72b0a1ff
Federico Aloi <federico.aloi@gmail.com>
2014-10-10 20:46:01 -0300

    Implementado cuantas_veces_recibiste

--- lib/a_descubrir.rb
+++ lib/a_descubrir.rb
@@ -2,14 +2,23 @@ require 'descubriendoMensajes/version'
 
 class ADescubrir
   class << self
-    attr_accessor :mensajes_recibidos
-  end
-
-  def method_missing(selector, *argumentos)
-    self.class.mensajes_recibidos << selector
+    attr_accessor :mensajes
   end
 
   def self.inherited(subclass)
-    subclass.mensajes_recibidos = Set.new
+    subclass.mensajes = Hash.new
+    subclass.mensajes.default = 0
+  end
+
+  def self.mensajes_recibidos
+    self.mensajes.keys
+  end
+
+  def self.cuantas_veces_recibiste(selector)
+    self.mensajes[selector]
+  end
+
+  def method_missing(selector, *argumentos)
+    self.class.mensajes[selector] = self.class.mensajes[selector] + 1
   end
 end
--- spec/a_descubrir_spec.rb
+++ spec/a_descubrir_spec.rb
@@ -12,15 +12,25 @@ RSpec.describe "ADescubrir" do
     lassie.correr
     lassie.correr
 
-    expect(Perro.mensajes_recibidos).to eq([:ladrar, :correr].to_set)
+    expect(Perro.mensajes_recibidos).to eq([:ladrar, :correr])
   end
 
   it "puede discriminar los mensajes segun su clase" do
     Perro.new.ladrar
     Gato.new.maullar
 
-    expect(Perro.mensajes_recibidos).to eq([:ladrar].to_set)
-    expect(Gato.mensajes_recibidos).to eq([:maullar].to_set)
+    expect(Perro.mensajes_recibidos).to eq([:ladrar])
+    expect(Gato.mensajes_recibidos).to eq([:maullar])
   end
 
+  it "sabe cuantas veces fue enviado cada mensaje" do
+    lassie = Perro.new
+    toby = Perro.new
+    lassie.ladrar
+    toby.ladrar
+    toby.ladrar
+
+    expect(Perro.cuantas_veces_recibiste(:ladrar)).to eq(3)
+    expect(Perro.cuantas_veces_recibiste(:correr)).to eq(0)
+  end
 end
